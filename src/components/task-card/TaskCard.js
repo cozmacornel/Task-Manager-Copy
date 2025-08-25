1624e4d0b6fee94b706535ad0a040305d849e42b
Cornel <cornel@example.com>
2025-08-25 21:23:15 +0300

    Modificare text card: am reusit sa fac primul proiect in react

--- src/components/task-card/TaskCard.js
+++ src/components/task-card/TaskCard.js
@@ -5,12 +5,13 @@ import DateContainer from "../date-container/DateContainer";
 function TaskCard(props) {
   return (
     <div className="card-wrapper">
+      <div className="card-top-line" />
       <div className="card-header">
         <p className="task-id">{props.id}</p>
         <Badge status={props.status} />
       </div>
       <div className="card-content">
-        <p>{props.name}</p>
+    <p>am reusit sa fac primul proiect in react</p>
       </div>
       <div className="card-footer">
         <DateContainer date={props.dueDate} />
